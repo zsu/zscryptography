1138058e4611081c42452dfa6cea155920d65872
zsu <azure.ss@gmail.com>
2024-09-25 20:55:30 -0400

    Refactor

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     author='ZSU',
     author_email='',
-    description='A simple cryptography library using AES and PKCS7 padding',
+    description='A simple cryptography library using AES',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/zsu/zscryptography',
